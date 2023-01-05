5e8a2d08314616562aa4b8c135f6f0fd898f342e
nazhimkalam <nazhimkalam@gmail.com>
2023-01-05 12:45:42 +0530

    created the how it works section

--- Implementation/frontend/src/pages/landing/LandingPage.tsx
+++ Implementation/frontend/src/pages/landing/LandingPage.tsx
@@ -2,18 +2,18 @@ import styled from "styled-components";
 import About from "../../components/About/About";
 import Footer from "../../components/Footer/Footer";
 import Header from "../../components/Header/Header";
+import HowItWorks from "../../components/HowItWorks/HowItWorks";
 
 const LandingPage = () => {
   return (
     <StyledContainer>
-      {/* header */}
       <Header />
 
       {/* title and about (given that it also gives the sentiment) */}
       <About />
 
       {/* how it works for both general users and domain specific users */}
-      <h2>How it works</h2>
+      <HowItWorks />
 
       {/* the summarizer tool to be used */}
       <h2>Summarizer</h2>
