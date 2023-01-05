52de4fd69f12066570a0b24371bf2c439baa5ff7
nazhimkalam <nazhimkalam@gmail.com>
2023-01-05 17:20:13 +0530

    creating a review list

---
+++ Implementation/frontend/src/components/Reviews/ReviewText.tsx
@@ -0,0 +1,74 @@
+import styled from "styled-components";
+
+const ReviewText = (props: any) => {
+  const { details } = props;
+  const { id, review, summary, counter, createdAt } = details;
+
+  const onHandleDeleteReview = (reviewId: string) => {
+
+  }
+
+  return (
+    <StyledContainer>
+      <section>
+        <h1>Review {counter}</h1>
+        <p>
+          <strong>Review: </strong>
+          {review}
+        </p>
+
+        <p>
+          <strong>Summary: </strong>
+          {summary}
+        </p>
+
+        <p>
+          <strong>Created At: </strong>
+          {createdAt}
+        </p>
+      </section>
+      <section>
+        <button onClick={() => onHandleDeleteReview(id)}>DELETE</button>
+      </section>
+    </StyledContainer>
+  );
+};
+
+export default ReviewText;
+
+const StyledContainer = styled.div`
+  margin: 1pc 0;
+  padding: 1pc 0;
+
+  display: flex;
+  justify-content: space-between;
+  align-items: end;
+  box-shadow: 0 1px 3px 0 rgba(60, 64, 67, 0.3),
+    0 4px 8px 3px rgba(60, 64, 67, 0.15);
+
+  > section {
+    margin: 0 1pc;
+
+    > h1 {
+      margin: 1pc 0;
+    }
+
+    > p {
+      margin: 0.5pc 0;
+    }
+
+    > button {
+      margin: 1pc 0;
+      padding: 0.5pc 1pc;
+      border: 1px solid red;
+      background-color: red;
+      cursor: pointer;
+      color: white;
+
+      &:hover {
+        background-color: white;
+        color: red;
+      }
+    }
+  }
+`;
--- Implementation/frontend/src/components/Reviews/Reviews.tsx
+++ Implementation/frontend/src/components/Reviews/Reviews.tsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import styled from "styled-components";
+import ReviewText from "./ReviewText";
 
 const Reviews = () => {
+    const reviews = [
+        {
+            id: "546asd5f4asdf6521dsf",
+            review: "This is a review",
+            summary: "This is a summary",
+            counter: 1,
+            createdAt: "2021-01-01",
+        },
+        {
+            id: '3845jksdhfgjsdfkghjksdh',
+            review: "This is a review",
+            summary: "This is a summary",
+            counter: 2,
+            createdAt: "2021-01-01",
+        },
+    ];
   return (
-    <div>Reviews</div>
+    <StyledContainer>
+        <h1>Reviews</h1>
+
+        <section>
+            {reviews.map((review, index) => (
+                <ReviewText key={index} details={review} />
+            ))}
+        </section>
+
+    </StyledContainer>
   )
 }
 
 export default Reviews
+
+const StyledContainer = styled.div`
+    margin: 1pc;
+    padding: 1pc;
+
+    > h1 {
+        margin: 1pc 0;
+    }
+
+    > section {
+        margin: 1pc 0;
+    }
+`;
