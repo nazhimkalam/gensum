2a9252375e84822fb86749853ddde7f5cceb0e2a
nazhimkalam <nazhimkalam@gmail.com>
2023-02-03 08:57:05 +0530

    implementation work

--- Implementation/backend/app.py
+++ Implementation/backend/app.py
@@ -3,6 +3,7 @@ from flask_restful import Api
 from flask import Flask, request
 import requests
 import transformers
+import pandas as pd
 import os
 import firebase_admin
 from firebase_admin import firestore, credentials
@@ -15,6 +16,7 @@ API = Api(app)
 FIREBASE_CREDENTIAL_CERTIFICATE = 'firebase/gatot-7b39d-firebase-adminsdk-bqeyf-0366f4bf3a.json'
 MODEL_NAME = 'bart-base_model'
 TOKENIZER_NAME = 'bart-base_tokenizer'
+GENERALIZED_DATASET_PATH = 'dataset/xsum.csv'
 GENERALIZED_MODEL_PATH = 'model/base/' + MODEL_NAME
 GENERALIZED_TOKENIZER_PATH = 'model/base/' + TOKENIZER_NAME
 MAX_INPUT = 512
@@ -29,6 +31,19 @@ firebase_credentials = credentials.Certificate(FIREBASE_CREDENTIAL_CERTIFICATE)
 firebaseApp = firebase_admin.initialize_app(firebase_credentials)
 firestoreDb = firestore.client()
 
+def query(payload):
+    headers = {"Authorization": "Bearer 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"}
+    response = requests.post(SENTIMENTAL_ANALYSIS_HUGGING_FACE_API_URL, headers=headers, json=payload)
+    return response.json()
+
+def getOverallSentimentWithScore(sentiment):
+    positiveScore = sentiment[0][0]['score']
+    negativeScore = sentiment[0][1]['score']
+    if positiveScore > negativeScore:
+        return 'Positive', positiveScore
+    else:
+        return 'Negative', negativeScore
+                
 @app.route('/', methods=['GET'])
 def hello_world():
     return {'message': 'Hello World'}, 200
@@ -42,20 +57,6 @@ def getGeneralizedSummary():
         outputs = generalized_model.generate(inputs, max_length=150, min_length=40, length_penalty=2.0, num_beams=4, early_stopping=True)
         summary = generalized_tokenizer.decode(outputs[0], skip_special_tokens=True)
 
-        headers = {"Authorization": "Bearer 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"}
-        def query(payload):
-            response = requests.post(SENTIMENTAL_ANALYSIS_HUGGING_FACE_API_URL, headers=headers, json=payload)
-            return response.json()
-
-        def getOverallSentimentWithScore(sentiment):
-            positiveScore = sentiment[0][0]['score']
-            negativeScore = sentiment[0][1]['score']
-            if positiveScore > negativeScore:
-                return 'Positive', positiveScore
-            else:
-                return 'Negative', negativeScore
-
-            
         sentimentAnalysisOutput = query({ "inputs": summary })
         sentiment, score = getOverallSentimentWithScore(sentimentAnalysisOutput)
         return {'summary': summary, 'sentment': {
@@ -81,30 +82,6 @@ def getUserData(userId):
     except Exception as e:
         return {'message': str(e)}, 500
 
-@app.route('/text-summarizer/domain', methods=['POST'])
-def getDomainSpecificSummary():
-    try:
-        data = request.get_json()
-        review = data['review']
-        userId = data['userId']
-        
-        folder_path = 'model/' + userId
-        model_path =  folder_path + '/' + MODEL_NAME
-        tokenizer_path = folder_path + '/' + TOKENIZER_NAME
-
-        if not os.path.exists(folder_path):
-            return {'message': "Model not found"}, 404
-
-        model = transformers.AutoModelForSeq2SeqLM.from_pretrained(model_path)
-        tokenizer = transformers.AutoTokenizer.from_pretrained(tokenizer_path)
-
-        inputs = tokenizer.encode(review, return_tensors='pt', max_length=MAX_INPUT, truncation=True)
-        outputs = model.generate(inputs, max_length=150, min_length=40, length_penalty=2.0, num_beams=4, early_stopping=True)
-        summary = tokenizer.decode(outputs[0], skip_special_tokens=True)
-        return {'summary': summary}, 200
-    except Exception as e:
-        return {'message': str(e)}, 500
-
 @app.route('/domain-profile-creation', methods=['POST'])
 def createDomainUserProfile():
     try:
@@ -125,18 +102,59 @@ def createDomainUserProfile():
     except Exception as e:
         return {'message': str(e)}, 500
 
+@app.route('/text-summarizer/domain', methods=['POST'])
+def getDomainSpecificSummary():
+    try:
+        data = request.get_json()
+        review = data['review']
+        userId = data['userId']
+        
+        folder_path = 'model/' + userId
+        model_path =  folder_path + '/' + MODEL_NAME
+        tokenizer_path = folder_path + '/' + TOKENIZER_NAME
+
+        if not os.path.exists(folder_path):
+            return {'message': "Model not found"}, 404
+
+        model = transformers.AutoModelForSeq2SeqLM.from_pretrained(model_path)
+        tokenizer = transformers.AutoTokenizer.from_pretrained(tokenizer_path)
+
+        inputs = tokenizer.encode(review, return_tensors='pt', max_length=MAX_INPUT, truncation=True)
+        outputs = model.generate(inputs, max_length=150, min_length=40, length_penalty=2.0, num_beams=4, early_stopping=True)
+        summary = tokenizer.decode(outputs[0], skip_special_tokens=True)
+
+        sentimentAnalysisOutput = query({ "inputs": summary })
+        sentiment, score = getOverallSentimentWithScore(sentimentAnalysisOutput)
+
+        firestoreDb.collection('domainUsers').document(userId).collection('reviewData').add({
+            'review': review,
+            'summary': summary,
+            'sentiment': sentiment,
+            'score': score
+        })
+
+        return {'summary': summary, 'sentment': {
+            'sentiment': sentiment,
+            'score': score
+        } }, 200
+    except Exception as e:
+        return {'message': str(e)}, 500
+
+
+
 @app.route('/domain-profile-retraining', methods=['POST'])
 def retrainDomainSpecifcModel():
     try:
         data = request.get_json()
         newReviewSummaryData = []
 
-        userId = data['userId'] # The user id is only need to save the model in the respective folder
+        userId = data['userId'] # The user id is only needed to save the model in the respective folder
         domainType = data['domainType'] # Using the domainType, we can get all the data from other users which have been given access for retraining
         isUseOtherData = data['isUseOtherData'] # we can have a radio button in the frontend to select if the user wants to retrain only with their data or with the other users data as well
 
         # Steps to be considered for retraining the model and dataset recreation
         # 1. By checking the isAccessible flag, we can decide whether to use the data for model retraining, then we get all the data from the database which isAccessible = true for the given domainType
+        print('Fetching data from the database...')
         if isUseOtherData == True:
             users = firestoreDb.collection('domainUsers').where('domainType', '==', domainType).where('isAccessible', '==', True).get()
             for user in users:
@@ -152,10 +170,26 @@ def retrainDomainSpecifcModel():
                     newReviewSummaryData.append(review.to_dict())
             else:
                 return {'message': "User not found"}, 404
-            
+        print('Successfully fetched data from the database')
+
+
         # 2. We create another dataframe with the generalized dataset we have and then we append the new data to the dataframe
+        print('Creating the dataframe using the fetched dataset...')
+        # rename the 'review' into 'document' in newReviewSummaryData
+        for review in newReviewSummaryData:
+            review['document'] = review.pop('review')
+
+        new_df = pd.DataFrame(newReviewSummaryData)
+        # old_df = pd.read_csv(GENERALIZED_DATASET_PATH)
+        # combined_df = pd.concat([new_df, old_df], axis=0) 
+        print('Successfully created the dataframe')
+
         # 3. We then perform the necessary preprocessing steps on the data and then we create the dataset
-        # 4. Since the new data is combined with the old data, we can start hyperparameter tuning
+        print('Preprocessing the dataset...')
+        # Here we will include the data preprocessing steps taken
+        print('Completed data preprocessing')
+
+        # 4. Since the new data is combined with the old data, we can start hyperparameter tuning and model retraining
         # 5. Once the hyperparameter tuning is done, we can save the model and tokenizer in the respective folder for the given userId
         # 6. Evaluation results needs to be stored in the database for the given userId and domainType for the model training
 
---
+++ Implementation/backend/utils/data_preprocessing.py
@@ -0,0 +1,58 @@
+from typing import Text, Dict, List, Union, Tuple, Optional, Set
+from constants import CHAT_WORDS_STR
+import re
+import contractions
+
+def md_links(text: Text) -> Text:
+    markdown_link=re.compile(r'\[.*?\]\(.*?\)')
+    return markdown_link.sub(r'',text)
+
+def scrape_links(text):
+    url = re.compile(r'https?://\S+|www\.\S+')
+    return url.sub(r'',text)
+
+def remove_html_tags(text: Text) -> Text:
+    html=re.compile(r'<.*?>')
+    return html.sub(r'',text)
+
+def chat_words_conversion(text: Text) -> Text:
+    # First, we're going to convert this long string into set of words and its shortcut
+    chat_words_map_dict = {}
+    chat_shortcut_list = set()
+    for line in CHAT_WORDS_STR.split("\n"):
+        if line != '':
+            shortcut = line.split('=')[0] # split the line from `=` sign and select shortcut
+            chat_words = line.split('=')[1]
+            chat_shortcut_list.add(shortcut) # add the chat  shortcut to the set
+            chat_words_map_dict[shortcut] = chat_words # add each chat_words corresponding to its shortcut
+
+    chat_words_map_dict
+
+    new_text = []
+    for word in text.split():
+        if word.upper() in chat_words_map_dict:
+            new_text.append(chat_words_map_dict[word.upper()])
+        else:
+            new_text.append(word)
+    return " ".join(new_text)
+
+def en_contractions(text: Text) -> Text:
+    return ' '.join([contractions.fix(word)
+                     if word in contractions.contractions_dict else word
+                     for word in text.split()])
+                     
+def preprocess_dataset(dataset):
+    def preprocess_column(col):
+        col = md_links(col)
+        col = scrape_links(col)
+        col = remove_html_tags(col)
+        col = chat_words_conversion(col)
+        return col
+
+    dataset['document'] = dataset['document'].apply(preprocess_column)
+    dataset['summary'] = dataset['summary'].apply(preprocess_column)
+
+
+
+
+
