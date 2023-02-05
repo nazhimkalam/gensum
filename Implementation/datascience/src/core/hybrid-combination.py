303c203a6ce525571acc7809cc259ed94fde8d2b
nazhimkalam <nazhimkalam@gmail.com>
2023-02-05 21:51:33 +0530

    completed the rich picture diagram

--- Implementation/datascience/src/core/hybrid-combination.py
+++ Implementation/datascience/src/core/hybrid-combination.py
@@ -1,58 +1,41 @@
+import torch
+import torch.nn as nn
 import transformers
-# using ensemble_transformers library for text summarization task using hybrid combination (BERT + GPT2 + XLNet)
-from ensemble_transformers import EnsembleModelForSequenceClassification
 
-bart_model_path = 'model/base/bart-base_model'
-bart_tokenizer_path = 'model/base/bart-base_tokenizer'
+bart_model_name = 'facebook/bart-base'
+t5_model_name = 't5-base'
 
-t5_model_path = 'model/base/t5-base_model'
-t5_tokenizer_path = 'model/base/t5-base_tokenizer'
+bart_model = transformers.AutoModelForSeq2SeqLM.from_pretrained(bart_model_name)
+t5_model = transformers.AutoModelForSeq2SeqLM.from_pretrained(t5_model_name)
 
-gpt2_model_path = 'model/base/gpt2-base_model'
-gpt2_tokenizer_path = 'model/base/gpt2-base_tokenizer'
+class HybridCombination(nn.Module):
+    def __init__(self, bart_model, t5_model):
+        super().__init__()
+        self.bart_model = bart_model
+        self.t5_model = t5_model
 
-bert_model_path = 'model/base/bert-base_model'
-bert_tokenizer_path = 'model/base/bert-base_tokenizer'
+    def forward(self, input_ids, attention_mask, decoder_input_ids, decoder_attention_mask):
+        bart_output = self.bart_model(input_ids=input_ids, attention_mask=attention_mask, decoder_input_ids=decoder_input_ids, decoder_attention_mask=decoder_attention_mask)
+        t5_output = self.t5_model(input_ids=input_ids, attention_mask=attention_mask, decoder_input_ids=decoder_input_ids, decoder_attention_mask=decoder_attention_mask)
+        output = bart_output + t5_output
+        return output
+    
+hybrid_model = HybridCombination(bart_model, t5_model)
 
-xlnet_model_path = 'model/base/xlnet-base_model'
-xlnet_tokenizer_path = 'model/base/xlnet-base_tokenizer'
-
-# load the models
-bart_model = transformers.BartForConditionalGeneration.from_pretrained(bart_model_path)
-
-t5_model = transformers.T5ForConditionalGeneration.from_pretrained(t5_model_path)
-
-gpt2_model = transformers.GPT2LMHeadModel.from_pretrained(gpt2_model_path)
-
-bert_model = transformers.BertForMaskedLM.from_pretrained(bert_model_path)
-
-xlnet_model = transformers.XLNetLMHeadModel.from_pretrained(xlnet_model_path)
-
-# load the tokenizers
-bart_tokenizer = transformers.BartTokenizer.from_pretrained(bart_tokenizer_path)
-
-t5_tokenizer = transformers.T5Tokenizer.from_pretrained(t5_tokenizer_path)
-
-gpt2_tokenizer = transformers.GPT2Tokenizer.from_pretrained(gpt2_tokenizer_path)
-
-bert_tokenizer = transformers.BertTokenizer.from_pretrained(bert_tokenizer_path)
-
-xlnet_tokenizer = transformers.XLNetTokenizer.from_pretrained(xlnet_tokenizer_path)
-
-# create the ensemble model
-ensemble_model = EnsembleModelForSequenceClassification(
-    models=[bart_model, t5_model],
-    tokenizer=[bart_tokenizer, t5_tokenizer],  
-    model_type='bart',
-    task='summarization',
-    model_weights=[0.2, 0.2, 0.2, 0.2, 0.2],
-    config=None,
-    device='cuda'
-)
-# summarize the text
+#  use the hybrid model to generate the summary of the text
 text = "The quick brown fox jumps over the lazy dog."
+tokenizer = transformers.AutoTokenizer.from_pretrained(bart_model_name)
+input_ids = tokenizer(text, return_tensors='pt').input_ids
+decoder_input_ids = tokenizer('summarize: ' + text, return_tensors='pt').input_ids
+output = hybrid_model.generate(input_ids, decoder_input_ids=decoder_input_ids)
+print(tokenizer.decode(output[0]))
 
-summary = ensemble_model(text, num_beams=4, max_length=100, early_stopping=True)
+#  use the hybrid model to generate the summary of the text
+text = "The quick brown fox jumps over the lazy dog."
+tokenizer = transformers.AutoTokenizer.from_pretrained(t5_model_name)
+input_ids = tokenizer(text, return_tensors='pt').input_ids
+decoder_input_ids = tokenizer('summarize: ' + text, return_tensors='pt').input_ids
+output = hybrid_model.generate(input_ids, decoder_input_ids=decoder_input_ids)
+print(tokenizer.decode(output[0]))
 
-print(summary)
-
+#  use the hybrid model to generate the summary of the text
